dde9bd651e6f003892e1961d2334738636c4ebac
BearmanAdmin <william.k97430@gmail.com>
2023-12-01 11:26:49 +0100

    env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: "https://polygon-testnet.public.blastapi.io/",
+      chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY as string], // Remplacez par la clé privée de votre compte Ethereum
+    },
+  },
 };
-
 export default config;
